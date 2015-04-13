2de3b695f3eedbba45d384a3d9e95529506684c4
mathiastoserve <mathias4u@gmx.net>
2015-04-13 11:06:29 +0200

    Added buttons to register and unregister

---
+++ .vertx/file-cache-dcd100e5-9798-4fc9-a03e-aa415bda2deb/webroot/js/app.js
@@ -0,0 +1,60 @@
+'use strict';
+
+angular.module('myApp', [ 'ngRoute', 'knalli.angular-vertxbus' ]).config(
+		[ '$routeProvider', 'vertxEventBusProvider',
+				function($routeProvider, vertxEventBusProvider) {
+					$routeProvider.when('/', {
+						templateUrl : 'views/main.html',
+						controller : 'MainCtrl'
+					})
+
+					vertxEventBusProvider.enable().useReconnect();
+				} ]);
+
+angular.module('myApp').controller(
+		'MainCtrl',
+		[
+				'$scope',
+				'vertxEventBusService',
+				function($scope, vertxEventBusService) {
+					$scope.main = this;
+					$scope.main.state = 'disconnected';
+					$scope.main.output = '';
+
+					$scope.$on('vertx-eventbus.system.disconnected', function(
+							event) {
+						$scope.main.state = 'disconnected';
+						console.log('disconnected');
+					});
+
+					$scope.$on('vertx-eventbus.system.connected', function(
+							event) {
+						$scope.main.state = 'connected';
+						console.log('connected');
+
+						// unregister current listener
+						unregisterBusListener(vertxEventBusService);
+
+						// register Listener
+						registerBusListener(vertxEventBusService, function(message){
+							$scope.main.output = $scope.main.output + '<br> '+message; 
+						});
+					});
+				} ]);
+
+var unregisterfn = null;
+
+function registerBusListener(vertxEventBusService, callback) {
+	unregisterfn = vertxEventBusService.on('outbound.test', function(message) {
+		console.log('<<<<<<<<<< ', message);
+		callback(message);
+	});
+	console.log('Listener registered');
+}
+
+function unregisterBusListener(vertxEventBusService) {
+	if (typeof unregisterfn === 'function') {
+		vertxEventBusService.removeListener('outbound.test', unregisterfn);
+		console.log('Listener unregistered');
+	}
+}
--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Reproducer for https://github.com/knalli/angular-vertxbus/issues/52
 3. Click the button "Register"
 4. Restart the server
 5. Client automatically reconnects and registers the handler, so far so good
-6. CLick the button "Unregister"
+6. Click the button "Unregister"
 7. Issue 1: client still receives messages from the server
-8. CLick the button "Unregister" again
+8. Click the button "Unregister" again
 9. Issue 2: client produces error a TypeError: Cannot read property 'length' of undefined
     at angular.module.provider.$get.wrapped.registerHandler.deconstructor  (http://localhost:8484/js/angular-vertxbus.js:647:42)
     at unregisterBusListener (http://localhost:8484/js/app.js:62:3)
